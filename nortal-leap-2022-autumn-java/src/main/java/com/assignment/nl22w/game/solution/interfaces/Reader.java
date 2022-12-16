fd48ebbb33c0e9d979924f1a853c1614dd9bc3fc
Oakroot <smicys.lukas@gmail.com>
2022-12-16 13:31:36 +0200

    cleaning mess

--- nortal-leap-2022-autumn-java/src/main/java/com/assignment/nl22w/game/solution/interfaces/Reader.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.nl22w.game.solution.interfaces;
-
-import org.springframework.core.io.Resource;
-
-public interface Reader {
-    String read(Resource resource);
-}
--- nortal-leap-2022-autumn-java/src/main/java/com/assignment/nl22w/game/solution/interfaces/Validator.java
+++
@@ -1,5 +0,0 @@
-package com.assignment.nl22w.game.solution.interfaces;
-
-public interface Validator {
-    boolean isValid(String text); //boolean is are should
-}
